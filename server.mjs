63127ffefd5fa41d330dce601d6694579ec1bc57
Valeri Sabev <valchygaming@gmail.com>
2022-06-01 22:39:42 +0200

    Bug fix - typo

--- server.mjs
+++ server.mjs
@@ -6,7 +6,7 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const baseDir = process.env.NODE_ENV === 'production' ? './dist' : '/.src';
+const baseDir = process.env.NODE_ENV === 'production' ? './dist' : './src';
 const app = express();
 
 // Custom cache headers for assets
